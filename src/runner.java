8876fb82c57f30f227f32b41ef3b09c8a115303e
BT_1N3_23 <sample@bths.edu>
2024-10-25 13:07:05 -0400

    is such bazinga yes

--- src/runner.java
+++ src/runner.java
@@ -1,5 +1,7 @@
 public class runner {
     public static void main(String[] args) {
         System.out.println("bazinga");
+        System.out.println("more bazinga");
+        System.out.println("even more bazinga");
     }
 }
